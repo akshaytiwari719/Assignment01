6d78ee8bcdea8d9dbe4bc1c97e91d061c9fd3c8b
akshaytiwari719 <63837126+akshaytiwari719@users.noreply.github.com>
2020-08-29 02:11:24 +0530

    add order table

--- database/migrations/2020_08_28_202728_create_order_table.php
+++ database/migrations/2020_08_28_202728_create_order_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('order', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('contact_No');
-            $table->string('address');
+            $table->date('order_Date');
+            $table->string('status');
+            $table->integer('user_Id')->unsigned();
+            $table->foreign('user_Id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('order');
     }
 }
